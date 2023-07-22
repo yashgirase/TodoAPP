c83a3754a39b9c6b9d86e3e3cdcae20e619d3ee6
Yashpal Rajput <yashpalgirase692@gmail.com>
2023-07-23 01:37:04 +0530

    This is my first commit

---
+++ ToDoAppBasic/src/main/java/com/geekster/TodoAPP/ToDoAppBasicApplication.java
@@ -0,0 +1,13 @@
+package com.geekster.TodoAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppBasicApplication.class, args);
+	}
+
+}
---
+++ ToDoAppBasic/src/test/java/com/geekster/TodoAPP/ToDoAppBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.TodoAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
